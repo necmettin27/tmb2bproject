d4d2b00225e0882ae618fb59dda8529bcb31045b
Necmettin Kartal <necmettin2748@gmail.com>
2023-03-13 19:55:51 +0300

    res

--- config/app.php
+++ config/app.php
@@ -176,8 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
-
-        Yajra\DataTables\DataTablesServiceProvider::class,
+ 
         Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
@@ -232,8 +231,7 @@ return [
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'View' => Illuminate\Support\Facades\View::class, 
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
